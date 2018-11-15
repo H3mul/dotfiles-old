4c06f1c1e47e49efdfdc0c3c33294a648da2058a
hemul <phil.d324@gmail.com>
2018-11-15 03:08:46 -0800

    added ohmyzsh install to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Create symlinks in home folder for all .sym files
 for s in $(find . -type f -name '*.sym'); do 
 	s=$(realpath $s)
 	f=${s%.sym}
@@ -7,3 +8,15 @@ for s in $(find . -type f -name '*.sym'); do
 	f="$HOME/$f"
 	ln -sf $s $f;	
 done
+
+
+#install oh-my-zsh if not already installed
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
+
+#install custom oh-my-zsh plugins if not already installed
+if [ ! -d  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
