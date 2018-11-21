b81d9ce963ceebb6d7f2306472ad3c1a2d98a895
hemul <phil.d324@gmail.com>
2018-11-20 22:14:07 -0800

    added install argument and progress messages

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-#install zsh
-if [ ! -f /bin/zsh ]; then 
-	sudo apt-get install zsh -y
-fi
+while getopts "i" opt; do
+	case $opt in
+		i)
+			echo "installing prerequisites..."
 
-#install ag
-if [ ! -f /usr/bin/ag ]; then 
-	sudo apt-get install silversearcher-ag -y
-fi
+			#install zsh
+			if [ ! -f /bin/zsh ]; then 
+				sudo apt-get install zsh -y
+			fi
+
+			#install ag
+			if [ ! -f /usr/bin/ag ]; then 
+				sudo apt-get install silversearcher-ag -y
+			fi
+			;;	
+	esac
+done
 
 #install oh-my-zsh if not already installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	echo "installing oh-my-zsh..."
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
+echo "creating symlinks to home directory..."
 # Create symlinks in home folder for all .sym files
 for s in $(find . -type f -name '*.sym'); do 
 	s=$(realpath $s)
@@ -29,6 +39,7 @@ done
 autosuggestions_home=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
 
 if [ ! -d  $autosuggestions_home ]; then
+	echo "installing autosuggestions for zsh..."
 	git clone https://github.com/zsh-users/zsh-autosuggestions $autosuggestions_home 
 fi
 
@@ -36,6 +47,7 @@ fi
 syntax_highlighting_home=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 if [ ! -d $syntax_highlighting_home ]; then
+	echo "installing syntax highlighting for zsh..."
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $syntax_highlighting_home 
 
 	chmod g-w $syntax_highlighting_home -R 
@@ -43,3 +55,4 @@ if [ ! -d $syntax_highlighting_home ]; then
 
 fi
 
+echo "Dotfiles deploy complete!"
