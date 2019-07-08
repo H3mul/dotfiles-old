9ac1f83b0f98335eea416ead16e9674e18146061
Phillip Dykman <phil.d324@gmail.com>
2019-07-08 00:26:40 -0700

    use gpg-agent for ssh

--- bin/ssh-agent.sh
+++ bin/ssh-agent.sh
@@ -1,4 +1,7 @@
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
-if [[ ! $(ps aux | grep ssh-agent | grep -v grep) ]]; then
-        eval $(ssh-agent)
-fi
+#!/bin/bash
+#export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+#if [[ ! $(ps aux | grep ssh-agent | grep -v grep) ]]; then
+#        eval $(ssh-agent)
+#fi
+
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
