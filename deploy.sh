dd9ffa287b80ef6c44860df37fd518c024e7451b
Phillip Dykman <phil.d324@gmail.com>
2019-09-30 21:39:55 -0700

    Revamped deployment mechanism + config for dolphin and konsole + prune

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
+# Chdir to dotfiles
 cd "$(dirname "$0")"
+
+cli=(
+    zsh
+    vim
+)
+
+plasma=(
+    konsole
+    dolphin
+)
+
+full=("${cli[@]} ${plasma[@]}")
+
+for item in ${full[@]}; do
+    stow -R -t "${HOME}" $item 2>/dev/null && echo "[+] Symlinks deployed: $item" || echo "[-] Symplinks failed: $item"
+done
+
+
+#######################################
+# Oh My ZSH
 
 #install oh-my-zsh if not already installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-        echo "installing oh-my-zsh..."
+        echo "[+] installing oh-my-zsh..."
         sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
-
-echo "creating symlinks to home directory..."
-# Create symlinks in home folder for all .sym files
-for s in $(find . -type f -name '*.sym'); do 
-        s=$(realpath $s)
-        f=${s%.sym}
-        f=${f##*/}
-        f="$HOME/$f"
-        ln -sf $s $f;   
-done
-
-echo "creating symlinks to custom zsh themes..."
-if [ -d "$HOME/.oh-my-zsh" ]; then
-    for s in $(find ./zsh -type f -name '*.zsh-theme'); do 
-            s=$(realpath $s)
-            f=${s##*/}
-            f="$HOME/.oh-my-zsh/themes/$f"
-            ln -sf $s $f;   
-    done
-fi
-
-#install custom oh-my-zsh plugins if not already installed
 
 autosuggestions_home=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
 
 if [ ! -d  $autosuggestions_home ]; then
-        echo "installing autosuggestions for zsh..."
+        echo "[+] installing autosuggestions for zsh..."
         git clone https://github.com/zsh-users/zsh-autosuggestions $autosuggestions_home 
 fi
 
@@ -41,7 +40,7 @@ fi
 syntax_highlighting_home=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 if [ ! -d $syntax_highlighting_home ]; then
-        echo "installing syntax highlighting for zsh..."
+        echo "[+] installing syntax highlighting for zsh..."
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $syntax_highlighting_home 
 
         chmod g-w $syntax_highlighting_home -R 
@@ -49,4 +48,8 @@ if [ ! -d $syntax_highlighting_home ]; then
 
 fi
 
-echo "Dotfiles deploy complete!"
+
+#######################################
+# Finished
+
+echo "[+] Dotfiles deploy complete!"
---
+++ konsole/.local/share/konsole/Shell.profile
@@ -0,0 +1,8 @@
+[Appearance]
+ColorScheme=Sweet
+Font=Droid Sans Mono,10,-1,5,50,0,0,0,0,0,Regular
+
+[General]
+Name=Shell
+Parent=FALLBACK/
+RemoteTabTitleFormat=%w
