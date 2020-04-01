957fc8a521c2170343ec4803fe4eff6df4a0f60b
Phillip Dykman <phil.d324@gmail.com>
2020-04-01 12:18:39 +0200

    Update scripts

--- bin/scripts/backup.sh
+++ bin/scripts/backup.sh
@@ -1,13 +1,25 @@
-cd / # THIS CD IS IMPORTANT THE FOLLOWING LONG COMMAND IS RUN FROM /
-backupfile="backup.$(date "+%Y.%m.%d-%H.%M.%S").tar.gz"
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
-tar -cvpzf /backups/$backupfile \
---exclude=/backups \
---exclude=/proc \
---exclude=/tmp \
---exclude=/mnt \
---exclude=/dev \
---exclude=/sys \
---exclude=/run \
---exclude=/media \
-/
+backupfile="backup.$(date "+%Y.%m.%d-%H.%M.%S").tar.gz"
+backupdir="/backups"
+backuppath="$backupdir/$backupfile"
+
+read -p "Start system backup to $backuppath? [y/n]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    cd / # THIS CD IS IMPORTANT THE FOLLOWING LONG COMMAND IS RUN FROM /
+    tar -cvpzf $backuppath \
+        --exclude=/backups \
+        --exclude=/proc \
+        --exclude=/tmp \
+        --exclude=/mnt \
+        --exclude=/dev \
+        --exclude=/sys \
+        --exclude=/run \
+        --exclude=/media \
+        /
+fi
+
